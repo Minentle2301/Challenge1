f052a1c06f8042c8c6ec40fa664eda30bbe45b3c
Minentle Mvunyiswa <102150619+Minentle2301@users.noreply.github.com>
2024-07-23 12:58:19 +0200

    Add files via upload

---
+++ Challenge1/if_else_statements.js
@@ -0,0 +1,11 @@
+let age = 21
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if (age < 20) {
+    console.log("You can not enter the club!")
+} else {
+    console.log("Welcome!")
+}
+
